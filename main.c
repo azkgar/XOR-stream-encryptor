f16af492a7a772e15e0090430af4768795095b23
azkgar <16088140+azkgar@users.noreply.github.com>
2026-06-29 19:05:50 -0400

    Add CLI parsing and error handling

--- main.c
+++ main.c
@@ -1,7 +1,11 @@
-/*
-* Rotate they key buffer left by 1 bit in-place.
-* Key: byte array of length key length
-*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**********************************************
+ * Rotate the key buffer left by 1 bit in-place
+ * Key: byte array of length key length
+ *********************************************/
 
 void rotateKeyLeft(uint8_t *key, size_t key_len)
 {
@@ -32,4 +36,61 @@ void rotateKeyLeft(uint8_t *key, size_t key_len)
 
     // Set last bit that wraps up from the 1st byte
     key[key_len - 1] |= firstBit;
+}
+
+
+/**
+ * 
+ */
+int main(int argc, char *argv[])
+{
+    // Variable for number of threads
+    int threads;
+    // Variable for key file
+    char *keyFile;
+    // Variable for catching the command prompt read result
+    int opt;
+
+    // Initialize threads to 1 as default value
+    threads = 1;
+    // Initialize key file to NULL
+    keyFile = NULL;
+
+    while((opt = getopt(argc, argv, "n:k:")) != -1)
+    {
+        switch (opt)
+        {
+            case 'n':
+                threads = atoi(optarg);
+                break;
+
+            case 'k':
+                keyFile = optarg;
+                break;
+
+            default:
+                fprintf(stderr,
+                        "Usage: %s -n threads -k keyfile\n",
+                        argv[0]);
+                return 1;
+        }
+    }
+
+    // Check if key file is provided
+    if(keyFile == NULL)
+    {
+        fprintf(stderr, 
+            "Error: key file is required (-k keyfile)\n");
+        return 1;
+    }
+
+    // Check if number of threads is valid
+    if(threads < 1)
+    {
+        fprintf(stderr,
+            "Error: number of threads must be greater or equal than 1\n");
+        return 1;
+    }
+
+    return 0;
 }
